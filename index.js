817915d0e4c5ba9b60e84d980cfe480755fea0b5
davidotieno <davidotieno473@gmail.com>
2023-10-06 11:30:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
-const cats=["Milo", "Otis", "Garfield"];
-console.log(cats);
-function destructivelyAppendCat(Ralph) {
-    cats.push(Ralph);
-    return cats;
-}
-function destructivelyPrependCat(Bob){
-    cats.unshift(Bob);
-    return cats;
-  }
-  function destructivelyRemoveLastCat(){
-    cats.pop();
-    return cats;
-  }
-  function destructivelyRemoveFirstCat(){
-    cats.shift();
-    return cats;
-  }
-  function appendCat(name){
-    var name_1= [...cats,name];
-    return name_1;
-  }
-  
-  function prependCat(name){
-    const name_2=[name, ...cats];
-    return name_2
-  }
-  
-  function removeLastCat(){
-    const newCats= cats.slice(0,cats.length-1);
-    return newCats;
-  }
-  
-  function removeFirsCat(){
-    const catss= cats.slice(1)
-    return catss;
-  }
+// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+ function destructivelyAppendCat(name){
+    cats;
+    console.log(cats.push(name))
+ }
+destructivelyAppendCat.push("Ralph");
+function destructivelyPrependCat(name){
+    cats;
+    console.log(cats.unshift(name))
+ }
+ destructivelyPrependCat.unshift("Bob");
+ function destructivelyPrependCat(name){
+    cats;
+    console.log(cats.unshift(name))
+ }
+ destructivelyPrependCat.unshift("Bob");
+ function destructivelyRemoveLastCat(name){
+    cats;
+    console.log(cats.pop(name))
+ }
+ destructivelyRemoveLastCat.pop();
+ function destructivelyRemoveFirstCat(name){
+    cats;
+    console.log(cats.shift(name))
+ }
+ destructivelyRemoveFirstCat.shift();
+ function appendCat(name){
+    const newCat = [...cats,name];
+    return newCat;
+ }
+ function prependCat(name){
+    const newCat = [name, ...cats];
+    return newCat;
+ }
+ function removeLastCat(){
+    const newCat= cats.slice(0,2);
+    return newCat;
+ }
+ function removeFirstCat(){
+    const newCat= cats.slice(1);
+    return newCat;
+ }
